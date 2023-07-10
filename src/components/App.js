dde554f1149fec39353a4aa1ae5b4e3839093458
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-07-10 01:56:53 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setImage(data.message));
+  }, []);
+
+  return (
+    <div>
+      {!image ? (
+        <p className="dog">Loading...</p>
+      ) : (
+        <>
+          <p className="dog">Enjoy!</p>
+          <img src={image} alt="A Random Dog" />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
